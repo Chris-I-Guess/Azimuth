1e0d717c6071795b4f1447b040e8c891fa5c7d1f
Chris-I-Guess <christianagoratsios@gmail.com>
2023-02-24 13:52:27 +1100

    Core Framework complete

---
+++ Azimuth/GameObjects/GameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Azimuth.GameObjects
+{
+	public abstract class GameObject
+	{
+		public Vector2 position;
+
+		public abstract void Load();
+		public abstract void Draw();
+		public abstract void Update(float _deltaTime);
+
+		public abstract void Unload();
+	}
+}
